3bb2b0b50ecd08d1078b41f7a7219e2c566c8f90
ksuka <ksikacheva@yandex.ru>
2025-06-15 14:57:47 +0300

    1page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){
 let sand = document.querySelector(".sand");
 let sand2 = document.querySelector(".sand2");
 
+sand.addEventListener("", function(){
+    sand.classList.toggle('visible');
+    
 
+
+})
 
 
 
