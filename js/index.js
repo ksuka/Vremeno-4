629df99c7fab0464217b40f924061518618948b1
ksuka <ksikacheva@yandex.ru>
2025-06-28 10:46:14 +0300

    ch

--- js/index.js
+++ js/index.js
@@ -117,16 +117,19 @@ function updateAllClocks() {
 window.addEventListener('DOMContentLoaded', updateAllClocks);
 
 
-//scroll
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+/*
+window.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-// create the scrollSmoother before your scrollTriggers
-ScrollSmoother.create({
-  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-  effects: true, // looks for data-speed and data-lag attributes on elements
-  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 1,
+    effects: true,
+    smoothTouch: 0.1
+  });
 });
-
+*/
 
 
 });
